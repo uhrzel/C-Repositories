94d916dfe6db16c12f0479197f08ef9d274c83ec
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-05-30 09:36:39 +0800

    neow

--- dict.cpp
+++ dict.cpp
@@ -4,5 +4,28 @@ using namespace std;
 int main()
 {
 
+    int grade = 0;
+
+    cout << "grade: " << endl;
+    cin >> grade;
+
+    if (grade >= 90)
+    {
+        cout << "Smart" << endl;
+    }
+    else if (grade > 80)
+
+    {
+        cout << "Excellent" << endl;
+    }
+    else if (grade > 70)
+    {
+        cout << "Passed" << endl;
+    }
+
+    else
+    {
+        cout << "Failed" << endl;
+    }
     return 0;
 }
