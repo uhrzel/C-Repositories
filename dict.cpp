d31105e43894bc18321c50716a0fa88a3c1c1bd5
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-06-01 11:54:44 +0800

    meow

--- dict.cpp
+++ dict.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void passValue(int array[], int target, int sz)
+{
+    int index1 = 0;
+    int index2 = 0;
+
+    for (int a = 0; a < sz; a++)
+    {
+        for (int b = 0; b < sz; b++)
+        {
+            if (array[b] + array[a] == target && b != a)
+            {
+                index1 = b;
+                index2 = a;
+            }
+        }
+    }
+
+    std::printf("[%d] [%d]", index1, index2);
+}
+
 int main()
 {
+
+    int array[] = {2, 3, 44, 5, 1};
+
+    int target = 98;
+
+    int size = sizeof(array) / sizeof(int);
+
+    passValue(array, target, size);
 
     return 0;
 }
