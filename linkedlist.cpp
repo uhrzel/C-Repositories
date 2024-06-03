58574094dac9a45810df49747cf2754e76e8c543
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-06-03 10:31:05 +0800

    node

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -6,6 +6,11 @@ struct dict
     int val = 0;
     dict *next;
 };
+struct node
+{
+    int value = 0;
+    node *next;
+};
 
 int main()
 {
@@ -13,18 +18,35 @@ int main()
     dict *second = new dict();
     dict *third = new dict();
 
+    node *one = new node();
+    node *two = new node();
+    node *three = new node();
+
     first->val = 1;
     second->val = 2;
     third->val = 3;
+
+    one->value = 1;
+    two->value = 2;
+    three->value = 3;
 
     first->next = second;
     second->next = third;
     third->next = NULL;
 
-    while (first != NULL)
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    while (first != NULL && one != NULL)
     {
-        cout << first->val << endl;
+        cout << first->val << " ";
         first = first->next;
+    }
+    while (one != NULL)
+    {
+        cout << one->value << " ";
+        one = one->next;
     }
 
     return 0;
